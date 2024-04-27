7868ed65ae0f1bea2781dd666151c2c4ff2543d1
Makcim Motruk <56037758+MakcFaktor@users.noreply.github.com>
2024-04-27 22:11:24 +0300

    mod script_5.3

--- js/script_5.3.js
+++ js/script_5.3.js
@@ -1,7 +1,21 @@
-let N = +prompt("Введіть число N:");
+// let N = +prompt("Введіть число N:");
+//
+// let limit = Math.sqrt(N);
+//
+// console.log(limit);
+//
+// for (let i = 1; i <= limit && i <= 100; i++) {
+//   let sum = i * i;
+//   console.log(`число ${i} квадрат цього числа ${sum}`);
+// }
 
-let limit = Math.sqrt(N);
+// -----------------------------------------------------------------
 
-for (let i = 1; i <= limit && i <= 100; i++) {
-  console.log(i * i);
+let N = parseInt(prompt("Введіть ціле число N:"));
+
+for (let i = 1; i <= 100; i++) {
+  let square = i * i;
+  if (square <= N) {
+    console.log(`число ${i} квадрат цього числа ${square}`);
+  }
 }
